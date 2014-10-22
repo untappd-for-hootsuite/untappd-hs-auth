1d96f1274b0f377282db135bc922a704fcc62c99
Toby Sullivan <toby.sullivan@hootsuite.com>
2014-10-21 21:19:27 -0700

    Read 'code' query param

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -4,9 +4,7 @@ import play.api.mvc._
 
 object Application extends Controller {
 
-  def callback = Action { req =>
-    val authToken = "not-set"
-
-    Redirect("http://untappdforhootsuite.com", Map("authToken" -> Seq(authToken)))
+  def callback(code: String) = Action { req =>
+    Redirect("http://untappdforhootsuite.com", Map("receivedCode" -> Seq(code)))
   }
 }
