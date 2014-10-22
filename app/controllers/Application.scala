bb56ff3d142088b409a6c32ee829b4cdd5119128
Toby Sullivan <toby.sullivan@hootsuite.com>
2014-10-21 21:45:38 -0700

    Exchange code for access token

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,10 +1,34 @@
 package controllers
 
+import play.api.Play
 import play.api.mvc._
+import play.api.libs.ws._
+import scala.concurrent.Future
 
 object Application extends Controller {
 
-  def callback(code: String) = Action { req =>
-    Redirect("http://untappdforhootsuite.com", Map("receivedCode" -> Seq(code)))
+  def callback(code: String) = Action.async { req =>
+    def getConfigString: String => String = Play.application.configuration.getString(_).getOrElse("")
+
+    val clientId = getConfigString("untappd.client_id")
+    val clientSecret = getConfigString("untappd.client_secret")
+    val redirectUrl = getConfigString("application.hostname") + "/callback"
+
+    // Exchange code for token
+    val resp: Future[WSResponse] = WS.url("https://untappd.com/oauth/authorize/")
+      .withQueryString(
+        "client_id" -> clientId,
+        "client_secret" -> clientSecret,
+        "response_type" -> "code",
+        "redirect_url" -> redirectUrl,
+        "code" -> code
+      )
+      .get()
+
+    resp.map { response =>
+      val token = ( response.json \ "response" \ "access_token" ).as[String]
+
+      Redirect("http://untappdforhootsuite.com", Map("receivedCode" -> Seq(code), "token" -> Seq(token)))
+    }
   }
 }
